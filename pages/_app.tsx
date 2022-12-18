b23495f10c8c2164783829056611511e741b2d23
jung-jin-lee <jungjin.lee.dev@gmail.com>
2022-12-18 23:10:17 +0900

    feat: add ga4 partytown example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Partytown } from '@builder.io/partytown/react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Partytown forward={['dataLayer.push', 'gtag']} />
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="worker"
+      ></Script>
+      <Script id="gtag-init" type="text/partytown">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          window.gtag = function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
